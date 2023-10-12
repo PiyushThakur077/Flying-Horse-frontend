ebd00583f452a9916561ee91285e2701e72ac879
amitrana0402 <amitrana0402@gmail.com>
2023-10-12 22:00:17 +0530

    fixes

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flying_horse/app/routes/app_pages.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../controllers/profile_controller.dart';
 
@@ -13,10 +15,16 @@ class ProfileView extends GetView<ProfileController> {
         title: const Text('ProfileView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
+      body: Center(
+        child: InkWell(
+          onTap: () {
+            GetStorage().erase();
+            Get.offAllNamed(Routes.LOGIN);
+          },
+          child: Text(
+            'Logout',
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
     );
